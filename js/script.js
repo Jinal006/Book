2ce6ac9a64f195997afac277f14630cf63c00ea9
Jinal006 <240130510018.cs@gujaratvidyapith.org>
2026-03-13 12:07:12 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function buy()
+{
+alert("Book added to cart");
+}
+
+function explore()
+{
+alert("Explore our book collection");
+}
---
+++ php/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<h1>User Login</h1>
+
+<form method="post">
+
+<input type="text" name="email" placeholder="Enter Email"><br><br>
+
+<input type="password" name="password" placeholder="Enter Password"><br><br>
+
+<button type="submit">Login</button>
+
+</form>
+
+</body>
+</html>
